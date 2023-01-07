19075f88b4446115b402760043e241e4f6a2f901
danielyabu21 <danielyabu21@gmail.com>
2023-01-07 10:58:04 -0300

    save todo on local storage

--- js/main.js
+++ js/main.js
@@ -6,7 +6,45 @@ const editCancelButton = document.getElementById("cancel-edit-btn");
 const toolbarEl = document.getElementById("toolbar");
 const todoFilter = document.getElementById("filter");
 const todoList = document.getElementById("todo-list");
+const todoDataBase = [];
 let oldEditInput;
+
+function setDataBase(data) {
+  return localStorage.setItem("todoList", JSON.stringify(data));
+}
+
+function getDataBase() {
+  return JSON.parse(localStorage.getItem("todoList"));
+}
+
+function createTodoElements(value) {
+  const todoContainer = document.createElement("li");
+  todoContainer.classList.add("todo");
+
+  const todoTitle = document.createElement("h3");
+  todoTitle.innerText = value;
+  todoContainer.appendChild(todoTitle);
+
+  const finishButton = document.createElement("button");
+  finishButton.classList.add("finish-todo");
+  finishButton.innerHTML = "<i class='bx bx-check'></i>";
+  todoContainer.appendChild(finishButton);
+
+  const editButton = document.createElement("button");
+  editButton.classList.add("edit-todo");
+  editButton.innerHTML = "<i class='bx bx-edit-alt'></i>";
+  todoContainer.appendChild(editButton);
+
+  const removeButton = document.createElement("button");
+  removeButton.classList.add("remove-todo");
+  removeButton.innerHTML = "<i class='bx bxs-tag-x'></i>";
+  todoContainer.appendChild(removeButton);
+
+  todoList.appendChild(todoContainer);
+  todoDataBase.push({
+    title: todoTitle.innerText,
+  });
+}
 
 function createTask(e) {
   e.preventDefault();
@@ -14,32 +52,11 @@ function createTask(e) {
   const addTodoInputValue = addTodoInput.value;
 
   if (addTodoInputValue) {
-    const todoContainer = document.createElement("div");
-    todoContainer.classList.add("todo");
-
-    const todoTitle = document.createElement("h3");
-    todoTitle.innerText = addTodoInputValue;
-    todoContainer.appendChild(todoTitle);
-
-    const finishButton = document.createElement("button");
-    finishButton.classList.add("finish-todo");
-    finishButton.innerHTML = "<i class='bx bx-check'></i>";
-    todoContainer.appendChild(finishButton);
-
-    const editButton = document.createElement("button");
-    editButton.classList.add("edit-todo");
-    editButton.innerHTML = "<i class='bx bx-edit-alt'></i>";
-    todoContainer.appendChild(editButton);
-
-    const removeButton = document.createElement("button");
-    removeButton.classList.add("remove-todo");
-    removeButton.innerHTML = "<i class='bx bxs-tag-x'></i>";
-    todoContainer.appendChild(removeButton);
-
-    todoList.appendChild(todoContainer);
-
+    createTodoElements(addTodoInputValue);
     addTodoInput.focus();
     addTodoInput.value = "";
+
+    setDataBase(todoDataBase);
   }
 }
 
@@ -66,7 +83,7 @@ function toggleForms() {
 addTodoForm.addEventListener("submit", createTask);
 document.addEventListener("click", ({ target }) => {
   let targetEl = target;
-  let parentTargetEl = target.closest("div");
+  let parentTargetEl = target.parentElement;
 
   let todoTitle;
 
@@ -77,10 +94,16 @@ document.addEventListener("click", ({ target }) => {
 
   if (targetEl.classList.contains("finish-todo")) {
     parentTargetEl.classList.toggle("done");
+    if (targetEl.hasAttribute("checked")) {
+      targetEl.removeAttribute("checked");
+    } else {
+      target.setAttribute("checked", "");
+    }
   }
 
   if (targetEl.classList.contains("edit-todo")) {
     oldEditInput = parentTargetEl.innerText;
+    console.log(oldEditInput);
     toggleForms();
   }
   if (targetEl.classList.contains("remove-todo")) {
@@ -97,4 +120,13 @@ editForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const editInputValue = editInput.value;
   editTask(editInputValue);
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  if (getDataBase()) {
+    const localData = getDataBase();
+    localData.forEach((item) => {
+      createTodoElements(item.title);
+    });
+  }
 });
